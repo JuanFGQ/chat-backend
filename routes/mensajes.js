04d71e709020eefe6422c7da7b56c5de4f030c74
Juan Felipe Garcia <juanFGQ@outlook.es>
2023-03-29 13:49:56 -0500

    Real Time Chat

--- routes/mensajes.js
+++ routes/mensajes.js
@@ -19,21 +19,19 @@ const { validarJWT } = require('../middlewares/validar-jwt');
 
 const router = Router();
 
-
-
-/*
-valida el token del usuario conectado y le retorna los usuarios 
-ala persona conectada 
-*/
-
 /*
 se añade el :de para saber quien es la persona que me interesa leer los mensajes
 */
 router.get('/:de',validarJWT,obtenerChat);//*118
+
+    
 
 
 
 
 
 module.exports = router;
+
+
+
 
